325db1a2e248107a45c64528a087c629abf38d7e
Luis Miguel <lmavendano1597@gmail.com>
2023-06-24 11:31:54 -0500

    next

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from './../../environments/environment.prod';
-// import { environment } from './../../environments/environment';
+// import { environment } from './../../environments/environment.prod';
+import { environment } from './../../environments/environment';
 import { Client, ClientResp } from '../interfaces/client.interface';
 import { tokenHeader } from '../utils/getConfig';
 
--- src/app/services/sale.service.ts
+++ src/app/services/sale.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment.prod';
-// import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { tokenHeader } from '../utils/getConfig';
 import { RowBuys, Sale, BuyResp } from '../interfaces/sales.interface';
